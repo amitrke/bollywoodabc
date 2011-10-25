abe512ad0aa21fa186fe5d3e9e7a6a810b61bdbe
amitrke@gmail.com <amitrke@gmail.com@44b91e78-070f-0b2d-ac69-6dc60ddc4a58>
2011-10-25 11:44:10 +0000

    Initial checkin

---
+++ trunk/BollywoodABC/WebApp/src/main/java/com/babc/server/model/TweetEntity.java
@@ -0,0 +1,145 @@
+package com.babc.server.model;
+
+import static com.babc.server.utils.EntityUtil.getDateProperty;
+import static com.babc.server.utils.EntityUtil.getIntegerProperty;
+import static com.babc.server.utils.EntityUtil.getLongProperty;
+import static com.babc.server.utils.EntityUtil.getStringProperty;
+import static com.babc.server.utils.EntityUtil.setProperty;
+
+import java.util.Date;
+
+import twitter4j.Status;
+
+import com.google.appengine.api.datastore.Entity;
+
+public class TweetEntity extends BaseEntityImpl {
+	
+	private static final long serialVersionUID = 1L;
+
+	private Date createdAt;
+	
+	private String text;
+	
+	private Long inReplyToStatusId;
+	
+	private Long inReplyToUserId;
+	
+	private int favorited;
+	
+	private String user;
+	
+	private int retweet;
+	
+	private int retweetedByMe;
+	
+	
+	public TweetEntity(Status status) {
+		this.createdAt = status.getCreatedAt();
+		this.text = status.getText();
+		this.inReplyToStatusId = status.getInReplyToStatusId();
+		this.inReplyToUserId = status.getInReplyToUserId();
+		
+		if (status.isFavorited()){
+			this.favorited = 1;
+		}
+		else{
+			this.favorited = 0;
+		}
+		
+		this.user = status.getUser().getScreenName();
+		
+		if (status.isRetweet()){
+			this.retweet = 1;
+		}
+		else{
+			this.retweet = 0;
+		}
+		
+		if (status.isRetweetedByMe()){
+			this.retweetedByMe = 1;
+		}
+		else{
+			this.retweetedByMe = 0;
+		}
+	}
+
+	@Override
+	public void save(Entity entity) {
+		super.save(entity);
+		setProperty(entity, "createdAt", createdAt, true);
+		setProperty(entity, "text", text, false);
+		setProperty(entity, "inReplyToStatusId", inReplyToStatusId, false);
+		setProperty(entity, "inReplyToUserId", inReplyToUserId, false);
+		setProperty(entity, "favorited", favorited, false);
+		setProperty(entity, "user", user, false);
+		setProperty(entity, "retweet", retweet, false);
+		setProperty(entity, "retweetedByMe", retweetedByMe, true);
+	}
+
+	@Override
+	public void load(Entity entity) {
+		super.load(entity);
+		createdAt = getDateProperty(entity, "createdAt");
+		text = getStringProperty(entity, "text");
+		inReplyToStatusId = getLongProperty(entity, "inReplyToStatusId");
+		inReplyToUserId = getLongProperty(entity, "inReplyToUserId");
+		favorited = getIntegerProperty(entity, "favorited", 0);
+		user = getStringProperty(entity, "user");
+		retweet = getIntegerProperty(entity, "retweet", 0);
+		retweetedByMe = getIntegerProperty(entity, "retweetedByMe", 0);
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public Long getInReplyToStatusId() {
+		return inReplyToStatusId;
+	}
+
+	public Long getInReplyToUserId() {
+		return inReplyToUserId;
+	}
+
+	public int getFavorited() {
+		return favorited;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public int getRetweet() {
+		return retweet;
+	}
+
+	public int getRetweetedByMe() {
+		return retweetedByMe;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TweetEntity [");
+		if (text != null) {
+			builder.append("text=");
+			builder.append(text);
+			builder.append(", ");
+		}
+		if (user != null) {
+			builder.append("user=");
+			builder.append(user);
+			builder.append(", ");
+		}
+		if (getId() != null) {
+			builder.append("getId()=");
+			builder.append(getId());
+		}
+		builder.append("]");
+		return builder.toString();
+	}	
+}
---
+++ trunk/BollywoodABC/WebApp/src/main/webapp/WEB-INF/jsp/admin/message.jsp
@@ -0,0 +1,4 @@
+<%@page isELIgnored="false" %> 
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:out value="${message}" />
